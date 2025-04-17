19e8bdff87b77351df766d1648b15cf8a6815280
msereini <msereini@bach-s45.wu-wien.ac.at>
2025-04-17 15:46:31 +0200

    use default_encoding for xmlrpc response body

--- src/ZPublisher/xmlrpc.py
+++ src/ZPublisher/xmlrpc.py
@@ -160,9 +160,13 @@ class Response(object):
         return delattr(self._real, name)
 
     def setBody(self, body, title='', is_error=0, bogus_str_search=None):
+        # xmlrpclib.dumps should use the encoding which is defined
+        # in zope.conf. now the encoding is under our control
+        encoding=default_encoding
         if isinstance(body, xmlrpclib.Fault):
             # Convert Fault object to XML-RPC response.
-            body = xmlrpclib.dumps(body, methodresponse=1, allow_none=True)
+            body = xmlrpclib.dumps(body, methodresponse=1, allow_none=True,
+                                    encoding=encoding)
         else:
             # Marshall our body as an XML-RPC response. Strings will be sent
             # strings, integers as integers, etc. We do *not* convert
@@ -172,7 +176,8 @@ class Response(object):
             # allow Nones nested inside data structures too.
             try:
                 body = xmlrpclib.dumps(
-                    (body,), methodresponse=1, allow_none=True)
+                    (body,), methodresponse=1, allow_none=True,
+                    encoding=encoding)
             except ConflictError:
                 raise
             except Exception:
@@ -180,7 +185,9 @@ class Response(object):
                 return
         # Set our body to the XML-RPC message, and fix our MIME type.
         self._real.setBody(body)
-        self._real.setHeader('content-type', 'text/xml')
+        # rfc3023 STRONGLY recomends setting the optional charset
+        self._real.setHeader(
+                    'content-type', 'text/xml; charset=%s' % (encoding,))
         return self
 
     def exception(self, fatal=0, info=None,
